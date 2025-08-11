815e640da34272c1eb0e65b43c1a66260be0d339
ZYKLOBS <type1512anonym@gmail.com>
2025-08-11 13:51:36 +0200

    changed address from localhost to 0.0.0.0

--- main.py
+++ main.py
@@ -962,4 +962,4 @@ if __name__ == "__main__":
     insert_llms()
     import uvicorn
     #Note on the server this should run on 0.0.0.0
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
